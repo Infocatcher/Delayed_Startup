feee1c221dd037f9871e8a438a9956d86a6bb609
Infocatcher <infocatcher.dev.null@gmail.com>
2014-05-08 12:37:29 +0400

    Initialize after loading of first window (window should be displayed before our startup)

--- bootstrap.js
+++ bootstrap.js
@@ -1,27 +1,42 @@
-var startupTimer;
+var delayedStartup, startupObserver, startupTimer;
+
 function install(params, reason) {
 }
 function uninstall(params, reason) {
 }
 function startup(params, reason) {
 	Components.utils.import("resource://gre/modules/Services.jsm");
-
-	var defBranch = Services.prefs.getDefaultBranch("");
-	defBranch.setIntPref("extensions.delayedStartup.initialDelay", 250);
-	defBranch.setBoolPref("extensions.delayedStartup.debug", false);
-
-	var initialDelay = Services.prefs.getIntPref("extensions.delayedStartup.initialDelay");
-	startupTimer = timer(function() {
+	function initPrefs() {
+		var defBranch = Services.prefs.getDefaultBranch("");
+		defBranch.setIntPref("extensions.delayedStartup.initialDelay", 50);
+		defBranch.setBoolPref("extensions.delayedStartup.debug", false);
+	}
+	function init() {
 		startupTimer = null;
 		Services.scriptloader.loadSubScript("chrome://delayedstartup/content/delayedStartup.js");
 		delayedStartup.init(reason);
-	}, initialDelay);
+	}
+	if(reason == APP_STARTUP) {
+		Services.obs.addObserver(startupObserver = function observer(subject, topic, data) {
+			Services.obs.removeObserver(observer, topic);
+			startupObserver = null;
+			subject.addEventListener("load", function load(e) {
+				subject.removeEventListener("load", load, false);
+				initPrefs();
+				var initialDelay = Services.prefs.getIntPref("extensions.delayedStartup.initialDelay");
+				startupTimer = timer(init, initialDelay);
+			}, false);
+		}, "domwindowopened", false);
+	}
+ 	else {
+		initPrefs();
+		init();
+	}
 }
 function shutdown(params, reason) {
-	if(startupTimer)
-		startupTimer.cancel();
-	else
-		delayedStartup.destroy(reason);
+	startupTimer    && startupTimer.cancel();
+	startupObserver && Services.obs.removeObserver(startupObserver, "domwindowopened");
+	delayedStartup  && delayedStartup.destroy(reason);
 }
 
 function timer(fn, delay) {
